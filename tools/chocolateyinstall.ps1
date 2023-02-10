1fb3be47b4a3881c3f4b5b5f331f6cc0e4c7c42c
Kotaro KISHIMOTO <kisimoto@allied-telesis.co.jp>
2023-02-10 20:14:53 +0900

    2.27.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/kmiya-culti/RLogin/files/6430623/rlogin_x32.zip'
-$url64      = 'https://github.com/kmiya-culti/RLogin/files/6430626/rlogin_x64.zip'
+$url        = 'https://github.com/kmiya-culti/RLogin/files/8972649/rlogin_x32.zip'
+$url64      = 'https://github.com/kmiya-culti/RLogin/files/8972650/rlogin_x64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'rlogin*'
 
-  checksum      = '36A0E8CDA9722F3C8CCC7F01779353D8F602024AA9CE2A0B6319E0E66DC13C92'
+  checksum      = '85835F30EFB42FE1F7E00BDADF4603DB8D1BAE95664EB3AAFC933D95F717A0FB'
   checksumType  = 'sha256'
-  checksum64    = 'B61A7126C2E710A7F117CE1F6A6BF56AC34069726E60C02B6D66C5165FE2A6A5'
+  checksum64    = '35903323ECAC1CFA3DA983C211FD972584AA51CEC8AEE73800609A2F3F7FA064'
   checksumType64= 'sha256'
 }
 
